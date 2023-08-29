1dc9e72d4a41e45880d3c41752664c518dab7efa
alexeynovosibirsk <nixoved@yandex.ru>
2023-08-29 21:50:34 +0700

    fixed group visual bug

---
+++ src/main/java/com/nazarov/radman/action/ListProcessingGroup.java
@@ -0,0 +1,15 @@
+package com.nazarov.radman.action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.nazarov.radman.util.ActionUtil;
+
+public class ListProcessingGroup extends DefaultActionGroup {
+
+    @Override
+    public void update(AnActionEvent e) {
+        // Set the availability based on opened filetype
+        e.getPresentation().setEnabledAndVisible(
+                ActionUtil.getDefaultExtension(e).equals("rad"));
+    }
+}
